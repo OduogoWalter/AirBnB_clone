9642133d5d23c8b74d41cb2e55d2d39ac6d489cf
OduogoWalter <oduogowalter@gmail.com>
2024-01-21 15:55:53 +0300

    base_model

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,8 @@
 """
 Module containing the BaseModel class.
 """
-from datetime import datetime 
 import uuid
-from models import storage
+from datetime import datetime
 
 class BaseModel:
     """
@@ -20,14 +19,20 @@ class BaseModel:
         """
         if kwargs:
             for key, value in kwargs.items():
-                if key != "__class__":
-                    if key == 'created_at' or key == 'updated_at':
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != '__class__':
                     setattr(self, key, value)
+            if 'id' not in kwargs:
+                setattr(self, 'id', str(uuid.uuid4()))
+            if 'created_at' not in kwargs:
+                setattr(self, 'created_at', datetime.now())
+            if 'updated_at' not in kwargs:
+                setattr(self, 'update_at', datetime.now())
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+            setattr(self, 'id', str(uuid.uuid4()))
+            setattr(self, 'created_at', datetime.now())
+            setattr(self, 'updated_at', datetime.now())
 
     def __str__(self):
         """
@@ -44,8 +49,7 @@ class BaseModel:
         Updates the public instance attribute
         updated_at with the current datetime.
         """
-        self.updated_at = datetime.now()
-        storage.save()
+        setattr(self, 'updated_at', datetime.now())
 
     def to_dict(self):
         """
