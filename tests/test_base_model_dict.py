dabbe8a2e23931c7ecfc3738b7097a13193a5d2e
OduogoWalter <oduogowalter@gmail.com>
2024-01-22 12:46:36 +0300

    changes

--- tests/test_base_model_dict.py
+++ tests/test_base_model_dict.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-"""
-Unit tests for BaseModel class with dictionary representation
-"""
-import unittest
 from models.base_model import BaseModel
 
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model.id)
+print(my_model)
+print(type(my_model.created_at))
+print("--")
+my_model_json = my_model.to_dict()
+print(my_model_json)
+print("JSON of my_model:")
+for key in my_model_json.keys():
+    print("\t{}: ({}) - {}".format(key,
+                                   type(my_model_json[key]),
+                                   my_model_json[key]))
 
-class TestBaseModelDict(unittest.TestCase):
-    def test_from_dict(self):
-        """Test creating a BaseModel instance from a dictionary"""
-        my_model = BaseModel()
-        my_model.name = "My_First_Model"
-        my_model.my_number = 89
-        my_model_json = my_model.to_dict()
+print("--")
+my_new_model = BaseModel(**my_model_json)
+print(my_new_model.id)
+print(my_new_model)
+print(type(my_new_model.created_at))
 
-        my_new_model = BaseModel(**my_model_json)
-        self.assertEqual(my_model.id, my_new_model.id)
-        self.assertEqual(my_model.name, my_new_model.name)
-        self.assertEqual(my_model.my_number, my_new_model.my_number)
-        self.assertEqual(str(my_model.created_at),
-                         str(my_new_model.created_at))
-        self.assertEqual(str(my_model.updated_at),
-                         str(my_new_model.updated_at))
-        self.assertEqual(str(my_model), str(my_new_model))
-
-
-if __name__ == '__main__':
-    unittest.main()
+print("--")
+print(my_model is my_new_model)
